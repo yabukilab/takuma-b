78a33a9b138d3dc1e33b26716b8bdfe1b4e93bfd
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-11 01:40:24 +0900

    user.sql

---
+++ htdocs/user.sql
@@ -0,0 +1,26 @@
+# (A1)mydbというデータベースが既にあるなら削除する（危険）．
+drop database if exists user;
+
+# (A2)mydbというデータベースを作る．
+create database user charset=utf8mb4;
+
+# (A3)ユーザ名testuser，パスワードpassでmydbにアクセスできるようにする．
+grant all on user.* to kou@localhost identified by 'fur1map@ss';
+
+# (A4)mydbを使う．
+use user;
+
+# (A5)table1というテーブルが既にあるなら削除する（危険）．
+drop table if exists users;
+
+create table users (
+  id int primary key auto_increment, # ここはいつも同じ
+  name varchar(64) not null,
+  email varchar(191) not null,
+  password varchar(191) not null# 最後にはカンマがないことに注意．
+);
+
+# (C1)データを作成する．
+insert into users (id, name, email, password) values
+(9, '田隈', takuma.hironori@it-chiba.ac.jp, $2y$10$XUd4erF.JuzPmOYwEuEhP.lOIXOqTqwiXm45mcr2EXn0XcNuYLThu),
+(11, 'kou', s1942121nn@s.chibakoudai.jp, $2y$10$ioiFSrDAgmS5CrRqoGh0i.dV/hg5ucHAUCLkvpbtnt6D8JgkrZQvG);
