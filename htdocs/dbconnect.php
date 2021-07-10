4331b4123b7467f963fc95ef91a24416e6168b58
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-11 03:26:38 +0900

    a

--- htdocs/dbconnect.php
+++ htdocs/dbconnect.php
@@ -1,10 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'user');
-define('DB_USER', 'kou');
-define('DB_PASS', "fur1map@ss");
-
+require_once 'env.php';
 
 function connect()
 {
