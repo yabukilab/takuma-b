7624522804b2e8e6ceebb988fcb483d81ca56091
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-11 01:49:41 +0900

    user.sql

--- htdocs/user.sql
+++ htdocs/user.sql
@@ -24,3 +24,20 @@ create table users (
 insert into users (id, name, email, password) values
 (9, '田隈', takuma.hironori@it-chiba.ac.jp, $2y$10$XUd4erF.JuzPmOYwEuEhP.lOIXOqTqwiXm45mcr2EXn0XcNuYLThu),
 (11, 'kou', s1942121nn@s.chibakoudai.jp, $2y$10$ioiFSrDAgmS5CrRqoGh0i.dV/hg5ucHAUCLkvpbtnt6D8JgkrZQvG);
+
+
+# (A5)table1というテーブルが既にあるなら削除する（危険）．
+drop table if exists product;
+
+create table product (
+  id int primary key auto_increment, # ここはいつも同じ
+  name varchar(64) not null,
+  email varchar(191) not null# 最後にはカンマがないことに注意．
+);
+
+# (C1)データを作成する．
+insert into users (id, name, email, ) values
+(17, 'textbook', s1942121nn@s.chibakoudai.jp),
+(18, '教科書', takuma.hironori@it-chiba.ac.jp),
+(23, 'textbook', s1942121nn@s.chibakoudai.jp),
+(30, '情報処理の教科書', s1942121nn@s.chibakoudai.jp);
