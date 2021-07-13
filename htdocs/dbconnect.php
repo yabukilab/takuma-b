f6274eaebbc6df718c7c69f3c8c4561aa6e5e4fe
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-13 23:47:59 +0900

    db

--- htdocs/dbconnect.php
+++ htdocs/dbconnect.php
@@ -8,10 +8,10 @@ function connect()
     $user = DB_USER;
     $pass = DB_PASS;
 
-    
+    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
     try {
-        $pdo = new PDO('mysql:host=$host; dbname=$db; charset=utf8mb4',$user,$pass, [
+        $pdo = new PDO($dsn, $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
