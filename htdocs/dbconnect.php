979c75cb62f718682eb410289d98951b6ac324dd
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-13 23:08:23 +0900

    dbconnect.php

--- htdocs/dbconnect.php
+++ htdocs/dbconnect.php
@@ -3,10 +3,10 @@ require_once 'env.php';
 
 function connect()
 {
-    $host ='DB_HOST';
-    $db   = 'DB_NAME';
-    $user = 'DB_USER';
-    $pass = 'DB_PASS';
+    $host =DB_HOST;
+    $db   = DB_NAME;
+    $user = DB_USER;
+    $pass = DB_PASS;
 
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
