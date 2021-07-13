b196952f9bcd05f5efe8ff44e862169dccebe185
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-13 23:44:30 +0900

    db

--- htdocs/dbconnect.php
+++ htdocs/dbconnect.php
@@ -11,7 +11,7 @@ function connect()
     
 
     try {
-        $pdo = new PDO('mysql:host=$host; dbname=$db; charset=utf8mb4''kou''furimapass', [
+        $pdo = new PDO('mysql:host=$host; dbname=$db; charset=utf8mb4',$user,$pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
