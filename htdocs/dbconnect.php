a3092c473f2abc2c663868440b75a2533b4d7081
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-13 19:01:48 +0900

    a

--- htdocs/dbconnect.php
+++ htdocs/dbconnect.php
@@ -18,12 +18,12 @@ function connect()
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
     try {
-        $pdo = new PDO($dsn, $user, $pass, [
+        $pdo = new PDO($dsn, $user, $pass);
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false)
-        ]);
-        return $pdo;
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        
+            return $pdo;
     } catch(PDOExeption $e) {
         echo '接続失敗です！'. $e->getMessage();
         exit();
