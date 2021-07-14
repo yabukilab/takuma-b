f0b171122bb356832810ae722d77e63aeb5997d3
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-15 00:01:43 +0900

    ul

--- htdocs/public/classes/UserLogic.php
+++ htdocs/public/classes/UserLogic.php
@@ -80,7 +80,7 @@ class UserLogic
     $arr[] = $email;
 
     try {
-      $stmt=function ->prepare($sql);
+      $stmt=function prepare($sql);
       $stmt->execute($arr);
       // SQLの結果を返す
       $user = $stmt->fetch();
