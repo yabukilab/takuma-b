d45d687f3b1ec07bf15fe50c879ed1b3425255f2
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-14 14:33:36 +0900

    db

--- htdocs/dbconnect.php
+++ htdocs/dbconnect.php
@@ -1,25 +1,24 @@
 <?php
-
+require_once 'env.php';
 
 function connect()
 {
-    $dbServer = '127.0.0.1';
-    $db   = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
-    $user = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'kou';
-    $pass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'furimapass';
+    $host =DB_HOST;
+    $db   = DB_NAME;
+    $user = DB_USER;
+    $pass = DB_PASS;
 
-    $dsn = "mysql:host={$dbServer};dbname={$db};charset=utf8";
+    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
     try {
-        $pdo = new PDO($dsn, $user, $pass);
-        
-    # プリペアドステートメントのエミュレーションを無効にする．
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    # エラー→例外
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       
+        $pdo = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+        return $pdo;
     } catch(PDOExeption $e) {
         echo '接続失敗です！'. $e->getMessage();
+        exit();
     }
 
 
@@ -27,4 +26,4 @@ function connect()
 
 
 
-   
+
