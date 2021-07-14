836bd7c28529b6ed0e25a4d51e0f9e73a4c8e99b
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-14 14:49:31 +0900

    dbc

--- htdocs/dbconnect.php
+++ htdocs/dbconnect.php
@@ -14,7 +14,7 @@ function connect()
     try {
         $pdo = new PDO($dsn, $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            
            ]);
            
         return $pdo;
