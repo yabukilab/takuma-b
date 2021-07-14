18f1fcae67a6d3b887ef9e25ba3bdcea9a890632
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-15 02:21:13 +0900

    ul

--- htdocs/public/classes/UserLogic.php
+++ htdocs/public/classes/UserLogic.php
@@ -22,7 +22,7 @@ class UserLogic
     $arr[] = password_hash($userData['password'], PASSWORD_DEFAULT);
 
     try {
-      $stmt = $_POST->prepare($sql);
+      $stmt = createUser($userData)->prepare($sql);
       $result = $stmt->execute($arr);
       return $result;
     } catch(\Exception $e) {
