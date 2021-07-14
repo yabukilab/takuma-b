d3e2d0f87da67fd738720b5edd821840c492bb83
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-15 01:38:32 +0900

    db

--- htdocs/db.php
+++ htdocs/db.php
@@ -1,33 +1,13 @@
 <?php
 
-/**
- * XSS対策：エスケープ処理
- * 
- * @param string $str 対象の文字列
- * @return string 処理された文字列
- */
-function h($str) {
-  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-}
-
-/**
- * CSRF対策
- * @param void
- * @return string $csrf_token
- */
-function setToken() {
-  // トークンを生成
-  // フォームからそのトークンを送信
-  // 送信後の画面でそのトークンを照会
-  // トークンを削除
-  $csrf_token = bin2hex(random_bytes(32));
-  $_SESSION['csrf_token'] = $csrf_token;
-
-  return $csrf_token;
-}
-
 # HTMLでのエスケープ処理をする関数（データベースとは無関係だが，ついでにここで定義しておく．）
-function connect(){
+function h($var) {
+  if (is_array($var)) {
+    return array_map('h', $var);
+  } else {
+    return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
+  }
+}
 
 $dbServer = '127.0.0.1';
 $dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
@@ -44,5 +24,4 @@ try {
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   echo "Can't connect to the database: " . h($e->getMessage());
-}
 }
