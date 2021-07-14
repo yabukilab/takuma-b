6830b8f6c03353d042a831bec35b88daa07bd8de
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-14 23:40:40 +0900

    ul

--- htdocs/public/classes/UserLogic.php
+++ htdocs/public/classes/UserLogic.php
@@ -118,3 +118,16 @@ class UserLogic
   }
 
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php require 'db.php'; ?>
+</body>
+</html>
