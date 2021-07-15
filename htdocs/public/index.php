05e6b5a78d44978cf3a8f8922c427a7cf59b00c8
koheiwakio <s1942121nn@s.chibakoudai.jp>
2021-07-15 18:57:58 +0900

    index

--- htdocs/public/index.php
+++ htdocs/public/index.php
@@ -86,7 +86,8 @@ $PData = getUserData($_GET);
 			<tbody>
 				<?php foreach($PData as $row): ?>
 					<tr>
-						<td><?php echo htmlspecialchars($row['name'], 'UTF-8') ?></td>
+						
+						<td><?php echo htmlspecialchars('ようこそ', ENT_QUOTES, 'UTF-8') ?></td>
 						<td><?php echo htmlspecialchars($row['email']) ?></td>
 						
 						
